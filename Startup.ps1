f9a972606a354d60c21738f87508ef6fe4cf469a
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-03-17 17:47:52 +0100

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -4,7 +4,7 @@ param (
     [string]$password
 )
 
-Invoke-Expression "C:\\AMT\\deploy_Sql_Express_choco.ps1.ps1 -adminpassword $password"
+Invoke-Expression "C:\\AMT\\deploy_Sql_Express_choco.ps1 -adminpassword $password"
 Invoke-Expression "C:\\AMT\\DeployCobol.ps1  -adminname $username -adminpassword $password"
      
 
