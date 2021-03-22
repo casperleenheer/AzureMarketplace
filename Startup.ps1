dd49293ba53ef14d35c760e66f5812d09a9c789d
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-03-22 15:28:44 +0100

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -10,4 +10,4 @@ Invoke-Expression "C:\\AMT\\deploy_Sql_Express_choco.ps1 -adminpassword $passwor
 Invoke-Expression "C:\\AMT\\deploy_step_3.ps1 -adminname $username -adminpassword $password" -Verbose
 Invoke-Expression "C:\\AMT\\FixSettings.ps1" -Verbose
 Invoke-Expression "C:\\AMT\\SetupAmt.ps1  -adminname $username -adminpassword $password" -Verbose
-Invoke-Expression "C:\\AMT\\InstallAmt_Local_Cobol_Core.ps1  -adminname $username -adminpassword $password" -Verbose
+Invoke-Expression "C:\\AMT\\InstallAmt_AzureMarket.ps1  -adminname $username -adminpassword $password" -Verbose
