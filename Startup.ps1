41e7df9273aac57d024a65891a0c9d744ec0b792
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-03-23 11:30:40 +0100

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -5,7 +5,7 @@ param (
     [string]$password
 )
 
-
+Invoke-Expression "C:\\AMT\\SetAccountAsService.ps1 -adminpassword $password" -Verbose
 Invoke-Expression "C:\\AMT\\deploy_Sql_Express_choco.ps1 -adminpassword $password" -Verbose
 Invoke-Expression "C:\\AMT\\FixSettings.ps1" -Verbose
 Invoke-Expression "C:\\AMT\\SetupAmt.ps1  -adminname $username -adminpassword $password" -Verbose
