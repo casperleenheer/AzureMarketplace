b18df2ee54d47ac15468567347b15d5eb80d75bf
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-03-30 14:29:51 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -6,7 +6,7 @@ param (
 )
 
 Invoke-Expression "C:\\AMT\\SetAccountAsService.ps1 -adminpassword $password" -Verbose
-Invoke-Expression "C:\\AMT\\deploy_Sql_Express_installer.ps1 -adminpassword $password" -Verbose
+Invoke-Expression "C:\\AMT\\deploy_Sql_Express_installer.ps1 -adminname $username -adminpassword $password" -Verbose
 Invoke-Expression "C:\\AMT\\FixSettings.ps1" -Verbose
 Invoke-Expression "C:\\AMT\\SetupAmt.ps1 -adminname $username -adminpassword $password" -Verbose
 Invoke-Expression "C:\\AMT\\InstallAmt_AzureMarket.ps1" -Verbose
