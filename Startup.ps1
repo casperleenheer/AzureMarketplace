f8c8bede60ed8854c3f0eb12baadb6131bdf6efa
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-04-01 18:22:51 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -6,7 +6,7 @@ param (
 )
 
 Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
-Invoke-Expression "C:\\AMT\\SetAccountAsService.ps1 -adminpassword $password" -Verbose
+Invoke-Expression "C:\\AMT\\SetAccountAsService.ps1 -adminname $username" -Verbose
 Invoke-Expression "C:\\AMT\\FixSettings.ps1" -Verbose
 Invoke-Expression "C:\\AMT\\SetupAmt.ps1 -adminname $username -adminpassword $password" -Verbose
 Invoke-Expression "C:\\AMT\\InstallAmt_AzureMarket.ps1" -Verbose
