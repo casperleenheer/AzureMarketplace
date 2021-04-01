e6eafdc016740bd26c73b2df51fdf3ec040a62cc
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-04-01 18:41:15 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -9,7 +9,7 @@ Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
 Invoke-Expression "C:\\AMT\\SetAccountAsService.ps1 -adminname $username" -Verbose
 Invoke-Expression "C:\\AMT\\FixSettings.ps1" -Verbose
 Invoke-Expression "C:\\AMT\\SetupAmt.ps1 -adminname $username -adminpassword $password" -Verbose
-New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Install AMT" -Value 'Powershell.exe -ExecutionPolicy Bypass -File "c:\AMT\InstallAmt_AzureMarket.ps1" -WindowStyle Hidden' -PropertyType "String" 
+New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Install AMT" -Value 'Powershell.exe -ExecutionPolicy Bypass -File "c:\AMT\InstallAmt_AzureMarket.ps1" -WindowStyle Normal' -PropertyType "String" 
 
 #Invoke-Expression "C:\\AMT\\InstallAmt_AzureMarket.ps1" -Verbose
 Stop-Transcript
