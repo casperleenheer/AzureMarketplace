33abf7014515741a8d9f342ac770a953d7e4c74a
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-04-07 09:46:06 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -8,7 +8,7 @@ param (
 
 Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
 #Invoke-Expression "C:\\AMT\\SetAccountAsService.ps1 -adminname $username" -Verbose
-Invoke-Expression "C:\\AMT\\FixSettings.ps1" -sqlserver $sqlserver -Verbose
+Invoke-Expression "C:\\AMT\\FixSettings.ps1 -sqlserver $sqlserver" -Verbose
 Invoke-Expression "C:\\AMT\\SetupAmt.ps1 -adminname $username -adminpassword $password" -Verbose
 
 #Add AMT install script as a run once script during first login.
