2fa187788f4d8cb197d464bdf59b3379a7567ef3
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-04-22 10:20:23 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -3,13 +3,15 @@ param (
   
     [string]$username,
     [string]$password,
-    [string]$sqlserver
+    [string]$sqlserver,
+    [string]$sqladmin,
+    [string]$sqlpassword
 )
 
 Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
 #Invoke-Expression "C:\\AMT\\SetAccountAsService.ps1 -adminname $username" -Verbose
 Invoke-Expression "C:\\AMT\\FixSettings.ps1 -sqlserver $sqlserver" -Verbose
-Invoke-Expression "C:\\AMT\\SetupAmt.ps1 -adminname $username -adminpassword $password" -Verbose
+Invoke-Expression "C:\\AMT\\SetupAmt.ps1 -adminname $username -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
 
 #Add AMT install script as a run once script during first login.
 New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Install AMT" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "c:\AMT\InstallAmt_AzureMarket.ps1" -WindowStyle Normal' -PropertyType "String" 
