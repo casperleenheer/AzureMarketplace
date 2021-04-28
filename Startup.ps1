5dba73bfc7f9586ae3fd17d946457104ba8ed6e4
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-04-28 16:43:55 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -6,8 +6,8 @@ param (
     [string]$sqlserver,
     [string]$sqladmin,
     [string]$sqlpassword,
-    [string[]]$online,
-    [string[]]$batch
+    [object[]]$online,
+    [object[]]$batch
 )
 
 Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
