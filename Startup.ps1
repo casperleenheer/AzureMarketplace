d798953c90f197318c391a31b9fa1cacd287853d
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-05-03 14:37:19 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -6,21 +6,25 @@ param (
     [string]$sqlserver,
     [string]$sqladmin,
     [string]$sqlpassword,
-    $amtSettings,
+    [string]$amtSettings,
     [switch]$allinone
 
 )
 
 Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
-Write-Host "->$amtSettings<-"
 
 
-$s = $amtSettings
+Invoke-Expression "C:\\AMT\\FixSettings.ps1 -sqlserver $sqlserver" -Verbose
+
 
 if (-not $allinone)
 {
   #Export of json with template goes wrong. All "" are gone.
   #Doing some replacements to make it json again.
+  
+  Write-Host "->$amtSettings<-"
+  
+  $s = $amtSettings
   $s = $s -replace '{', '{"'
   $s = $s -replace ':', '":"'
   $s = $s -replace '}', '"}'
@@ -34,23 +38,16 @@ if (-not $allinone)
 
   $json = ConvertFrom-Json -InputObject $s -ErrorAction Stop
   Write-Host $json
-}
-Invoke-Expression "C:\\AMT\\FixSettings.ps1 -sqlserver $sqlserver" -Verbose
 
-if ($allinone)
-{
-  Invoke-Expression "C:\\AMT\\SetupAllInOne.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
-}
-else
-{
   Invoke-Expression "C:\\AMT\\SetupAmt.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
   Invoke-Expression "C:\\AMT\\AdjustEnvironmentFile.ps1 -json $json" -Verbose
 }
-
-
+else 
+{
+  Invoke-Expression "C:\\AMT\\SetupAllInOne.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
+}
 
 #Add AMT install script as a run once script during first login.
-#New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Setup AMT" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "C:\\AMT\\SetupAmt.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -WindowStyle Normal' -PropertyType "String" 
-
 New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Install AMT" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "c:\AMT\InstallAmt_AzureMarket.ps1" -WindowStyle Normal' -PropertyType "String" 
+
 Stop-Transcript
