eeba8ca2b8a656e70f23bd082c56141a9239995f
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-05-04 15:49:31 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -13,10 +13,6 @@ param (
 
 Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
 
-
-Invoke-Expression "C:\\AMT\\FixSettings.ps1 -sqlserver $sqlserver" -Verbose
-
-
 if (-not $allinone)
 {
   pwsh.exe -ExecutionPolicy Unrestricted -File C:\\AMT\\AdjustEnvironmentFile.ps1 -jsonstring $amtSettings
@@ -27,6 +23,8 @@ else
 {
   Invoke-Expression "C:\\AMT\\SetupAllInOne.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
 }
+
+C:\AMT\FixSettings.ps1 -sqlserver $sqlserver
 
 #Add AMT install script as a run once script during first login.
 New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Install AMT" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "c:\AMT\InstallAmt_AzureMarket.ps1" -WindowStyle Normal' -PropertyType "String" 
