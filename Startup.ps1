5653d3c5c7bacc6c458b08d938ba25460336ba58
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2021-07-22 13:24:24 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -7,8 +7,7 @@ param (
     [string]$sqladmin,
     [string]$sqlpassword,
     [string]$amtSettings,
-    [switch]$allinone,
-    [boolean]$installAmt
+    [switch]$allinone
 )
 
 Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
@@ -17,26 +16,24 @@ Start-Transcript -Path "C:\amt\transcript.txt" -NoClobber
 C:\AMT\FixSettings.ps1 -sqlserver $sqlserver
 
 #There are two flavours: allinone, which has AMT and SQL on one box and flex which has multiple AMT boxes and a seperate SQL server
-if ($installAmt)
+
+if (-not $allinone)
 {
+  $amtSettings | Out-File "C:\AMT\AmtSettings.json"
+  $envSettings | Out-File "C:\AMT\EnvSettings.json"
+  $value = 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "C:\AMT\AdjustEnvironmentFile.ps1" -jsonstring ' + $amtSettings + ' -WindowStyle Normal -NoExit'
+  New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Adjust AMT xml" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "C:\AMT\AdjustEnvironmentFile.ps1" -WindowStyle Normal' -PropertyType "String" 
 
-  if (-not $allinone)
-  {
-    $amtSettings | Out-File "C:\AMT\AmtSettings.json"
-    $envSettings | Out-File "C:\AMT\EnvSettings.json"
-    $value = 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "C:\AMT\AdjustEnvironmentFile.ps1" -jsonstring ' + $amtSettings + ' -WindowStyle Normal -NoExit'
-    New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Adjust AMT xml" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "C:\AMT\AdjustEnvironmentFile.ps1" -WindowStyle Normal' -PropertyType "String" 
+  C:\AMT\SetupAmt.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword
 
-    C:\AMT\SetupAmt.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword
-
-  }
-  else 
-  {
-    Invoke-Expression "C:\\AMT\\SetupAllInOne.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
-  }
-
-
-  #Add AMT install script as a run once script during first login.
-  New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Install AMT" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "c:\AMT\InstallAmt_AzureMarket.ps1" -WindowStyle Normal' -PropertyType "String" 
 }
+else 
+{
+  Invoke-Expression "C:\\AMT\\SetupAllInOne.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
+}
+
+
+#Add AMT install script as a run once script during first login.
+New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce\" -Name "Install AMT" -Value 'pwsh.exe  -WorkingDirectory C:\AMT -ExecutionPolicy Bypass -File "c:\AMT\InstallAmt_AzureMarket.ps1" -WindowStyle Normal' -PropertyType "String" 
+
 Stop-Transcript
