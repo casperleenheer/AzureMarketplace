00f8440cf303dc9d743028549c56f0be4b412ca4
asyscogithub <41142132+asyscogithub@users.noreply.github.com>
2022-07-01 11:09:30 +0200

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -29,7 +29,7 @@ if (-not $allinone)
 }
 else 
 {
-  #Invoke-Expression "C:\\AMT\\SetupAllInOne.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
+  Invoke-Expression "C:\\AMT\\SetupAllInOne.ps1 -adminname $username -adminpassword $password -sqladminname $sqladmin -sqladminpassword $sqlpassword" -Verbose
 }
 
 
